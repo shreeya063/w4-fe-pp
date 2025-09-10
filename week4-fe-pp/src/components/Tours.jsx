15669d6e576fc82e4fdba58e3337d6ae63410494
Shreeya <shreeyap@metropolia.fi>
2025-09-10 14:21:02 +0300

    Iteration 3

--- week4-fe-pp/src/components/Tours.jsx
+++ week4-fe-pp/src/components/Tours.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import { tours } from "../data";
 import Title from "./Title";
 import Tour from "./Tour";
 
-const Tours = () => {
+function Tours() {
+  const [toursData, setToursData] = useState(tours);
+
   return (
     <section className="section" id="tours">
       <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
-        {tours.map((tour) => {
+        {toursData.map((tour) => {
           return <Tour key={tour.id} {...tour} />;
         })}
       </div>
