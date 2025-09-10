6b8dbe36a6af599c139cd5437c87974eae514729
Shreeya <shreeyap@metropolia.fi>
2025-09-10 14:11:05 +0300

    Iteration 2

--- week4-fe-pp/src/components/Services.jsx
+++ week4-fe-pp/src/components/Services.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import { services } from '../data'
 import Title from './Title'
 import Service from './Service'
 
+
 const Services = () => {
+  const [servicesData, setServicesData] = useState(services);
+
   return (
     <section className='section services' id='services'>
       <Title title='our' subTitle='services' />
 
       <div className='section-center services-center'>
-        {services.map((service) => {
+        {servicesData.map((service) => {
           return <Service {...service} key={service.id} />
         })}
       </div>
